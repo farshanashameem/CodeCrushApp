768b101e3cda546e2a9fe0ceb0c947047253db02
farshana <farshanakilliyanni@gmail.com>
2026-09-03 12:21:36 +0530

    fix: allow production frontend in cors

--- backEnd/src/Presentation/app.ts
+++ backEnd/src/Presentation/app.ts
@@ -13,10 +13,12 @@ app.use(
     pinoHttp({ logger })
 );
 
-app.use( cors ({
-     origin: 'http://localhost:5173',
-    credentials: true
-}));
+app.use(
+    cors({
+        origin: ['http://localhost:5173', 'https://codecrush.lol'],
+        credentials: true
+    })
+);
 
 app.use(express.json());
 app.use(cookieParser());
